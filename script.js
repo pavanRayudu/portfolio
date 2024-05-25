c2a6c8400b19f87fb3d523f4d09478dd0572bfda
Vamsi Rayudu <rpvvamsi@gmail.com>
2024-05-25 07:34:04 +0530

    dark theme implemented

---
+++ script.js
@@ -0,0 +1,16 @@
+const theme_button = document.querySelector('.theme-option');
+
+theme_button.addEventListener('click', (e) => {
+    let current_mode = document.documentElement.getAttribute('data-theme');
+    if (current_mode === 'light') {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        theme_button.style.backgroundImage = "url('./assets/sun-svgrepo-com.svg')";
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        theme_button.style.backgroundImage = "url('./assets/moon-svgrepo-com.svg')";
+
+    }
+})
+
+background: url('./assets/sun-svgrepo-com.svg');
